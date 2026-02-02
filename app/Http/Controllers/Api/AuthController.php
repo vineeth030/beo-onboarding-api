aab47bcd6d709f38e7996f7aee5ae12caf216ffa
Vineeth Vijayan <vineeth030@gmail.com>
2026-02-02 13:03:02 +0530

    Check if any active offer exists for the candidate while they login.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Actions\AuthenticateWithBEOSystem;
 use App\Http\Controllers\Controller;
 use App\Models\Activity;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Support\Facades\Log;
-
-use function PHPUnit\Framework\isNull;
 
 class AuthController extends Controller
 {
@@ -31,7 +27,7 @@ class AuthController extends Controller
 
         return response()->json([
             'message' => 'User registered successfully',
-            'user' => $user
+            'user' => $user,
         ], 201);
     }
 
@@ -42,12 +38,12 @@ class AuthController extends Controller
             'password' => 'required',
         ]);
 
-        $emailDomain = substr(strrchr($request->email, "@"), 1);
+        $emailDomain = substr(strrchr($request->email, '@'), 1);
 
         if ($emailDomain == 'beo.in' || empty($emailDomain)) {
 
             $authResponse = $this->adminAuth($request->input('email'), $request->input('password'));
-        }else{
+        } else {
 
             $authResponse = $this->employeeAuth($request->input('email'), $request->input('password'));
         }
@@ -55,25 +51,35 @@ class AuthController extends Controller
         return response()->json($authResponse);
     }
 
-    private function employeeAuth($email, $password): array{
-        
+    private function employeeAuth($email, $password): array
+    {
+
         $user = User::where('email', $email)->first();
 
-        if (!$user || !Hash::check($password, $user->password)) {
+        if (! $user || ! Hash::check($password, $user->password)) {
             throw ValidationException::withMessages([
                 'login' => ['The provided credentials are not correct.'],
             ]);
         }
 
+        // Check if the candidate has at least one active offer
+        if ($user->employee) {
+            $hasActiveOffer = $user->employee->offers()->where('is_revoked', false)->exists();
+
+            if (! $hasActiveOffer) {
+                abort(403, 'No active offers found, please contact the HR team');
+            }
+        }
+
         $token = $user->createToken($email)->plainTextToken;
 
-        if ($user->role != "superadmin") {
+        if ($user->role != 'superadmin') {
             Activity::create([
                 'employee_id' => $user->employee?->id,
                 'performed_by_user_id' => $user->id,
                 'user_type' => 'candidate',
                 'type' => 'candidate.login',
-                'title' => $user->employee?->first_name . ' ' . $user->employee?->last_name . ' logged in successfully.',
+                'title' => $user->employee?->first_name.' '.$user->employee?->last_name.' logged in successfully.',
             ]);
         }
 
@@ -82,45 +88,45 @@ class AuthController extends Controller
             'token' => $token,
             'role' => $user->role,
             'user_id' => $user->id,
-            'employee_id' => $user->employee?->id
+            'employee_id' => $user->employee?->id,
         ];
     }
 
     private function adminAuth($userName, $password): array
     {
         try {
-            [$sessionToken, $userIdCode, $message] = (new BEOSystemController())->login($userName, $password);
+            [$sessionToken, $userIdCode, $message] = (new BEOSystemController)->login($userName, $password);
         } catch (\Throwable $th) {
             return [
                 'message' => $th->getMessage(),
-                'sessionToken' => null
+                'sessionToken' => null,
             ];
         }
 
         if ($sessionToken == null) {
             return [
                 'message' => $message,
-                'sessionToken' => null
+                'sessionToken' => null,
             ];
         }
 
         try {
-            $adminDetails = (new BEOSystemController())->retrive($sessionToken, $userIdCode);
+            $adminDetails = (new BEOSystemController)->retrive($sessionToken, $userIdCode);
         } catch (\Throwable $th) {
             return [
                 'message' => $th->getMessage(),
-                'sessionToken' => null
+                'sessionToken' => null,
             ];
         }
 
         if ($adminDetails['group'] != 'Human Resources') {
             return [
                 'message' => 'Unauthorised access.',
-                'sessionToken' => null
+                'sessionToken' => null,
             ];
         }
 
-        //Update users table with this api response.
+        // Update users table with this api response.
         $user = User::firstOrCreate(
             ['email' => $adminDetails['email']], // search condition
             ['name' => $adminDetails['name'], 'password' => Hash::make($password), 'role' => 'admin'] // values if not found
@@ -135,7 +141,7 @@ class AuthController extends Controller
             'role' => 'admin',
             'user_id' => $user->id,
             'employee_id' => 0,
-            'userIdCode' => $userIdCode
+            'userIdCode' => $userIdCode,
         ];
     }
 
